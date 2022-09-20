75734ea5d9d42a7bad9a87ca0ec9bfa610e8fc11
Max Chamberlain <maxchamberlain@Maxs-MacBook-Pro.local>
2022-09-20 10:05:08 -0600

    init

--- controllers/returnsController.js
+++ controllers/returnsController.js
@@ -45,9 +45,9 @@ const updateReturns = async (req, res) => {
             $set: {
                 ...req.body,
                 updatedAt: new Date().toISOString(),
-                closed: req.body.state === 'closed' ? new Date().toISOString() : exists.closed,
-                started_transit: req.body.label_status === 'in-transit' ? new Date().toISOString() : exists.started_transit,
-                received: req.body.label_status === 'N/A' ? new Date().toISOString() : exists.received,
+                closed: req.body.state === 'closed' ? new Date().toISOString() : exists.closed || false,
+                started_transit: req.body.label_status === 'in-transit' ? new Date().toISOString() : exists.started_transit || false,
+                received: req.body.label_status === 'N/A' ? new Date().toISOString() : exists.received || false,
             }
         });
         console.log('returns updated')
