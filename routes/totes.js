db3ea15605e7574f7cee0b080c6b2fb4e942e6e8
Max Chamberlain <maxchamberlain@Maxs-MacBook-Pro.local>
2022-10-03 11:39:42 -0600

    tote data

--- routes/totes.js
+++ routes/totes.js
@@ -4,7 +4,10 @@ const { getTotes, insertTotes } = require('../controllers/toteDataController');
 
 router.post('/:id/insert', insertTotes)
 router.get('/:id/insert', async function(req, res){
-    res.status(200).send({ message: 'Success' });
+    res.status(200).send({
+        "code": "200",
+        "Status": "Success"
+      });
 })
 router.post('/get', getTotes)
 
