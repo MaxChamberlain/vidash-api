ca3ac20ea0efe2226a67ef756eb9c852b73bb83b
Max Chamberlain <maxchamberlain@Maxs-MacBook-Pro.local>
2022-10-03 11:27:52 -0600

    tote data

--- routes/totes.js
+++ routes/totes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const { getTotes, insertTotes } = require('../controllers/toteDataController');
 
 router.post('/:id/insert', insertTotes)
+router.get('/:id/insert', async function(req, res){
+    res.status(201).send({ message: 'Success!' });
+})
 router.post('/get', getTotes)
 
 module.exports = router;
