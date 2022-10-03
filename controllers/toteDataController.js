d735964c46532ae28ae2435d744df0f20dcb88b1
Max Chamberlain <maxchamberlain@Maxs-MacBook-Pro.local>
2022-10-03 11:20:07 -0600

    tote data

--- controllers/toteDataController.js
+++ controllers/toteDataController.js
@@ -16,18 +16,12 @@ const getTotes = async (req, res) => {
 }
 
 const insertTotes = async (req, res) => {
-    const { totes } = req;
     const company_code = req.params.id;
-    totes.createdAt = new Date().toISOString();
-    totes.updatedAt = new Date().toISOString();
     const client = getClient();
     const db = client.db();
     const collection = db.collection('totes-' + company_code);
-    const exists = await collection.findOne({ id: totes.id });
-    if(!exists){
-        const data = await collection.insertOne(totes);
-        console.log('totes inserted')
-    }
+    const data = await collection.insertOne({...totes, createdAt: new Date().toISOString(), updatedAt: new Date().toISOString()});
+    console.log('totes inserted')
     res.status(201).send({ message: 'Success!' });
 }
 
